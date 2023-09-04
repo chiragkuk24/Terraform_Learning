8769ec6accdd347d375dd335515e12eebb311dce
Chirag Kukreja <chirag.kukreja24@gmail.com>
2023-09-04 20:53:42 +0530

    Provider script

---
+++ 001_Providers.tf
@@ -0,0 +1,13 @@
+## This Script is the Provider Script for Terraform with AWS
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.15.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
