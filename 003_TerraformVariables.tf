437ee2215ab8159acc7d4e51304a9ffc6c79d81a
Chirag Kukreja <chirag.kukreja24@gmail.com>
2023-09-07 23:06:52 +0530

    Terraform Variables

--- 003_TerraformVariables.tf
+++ 003_TerraformVariables.tf
@@ -1,10 +1,10 @@
 #Declaring Variables
 variable "myfirstvar"{
-        defaut=2
+        default=2
 }
 
 #Printing the Variable Value
 
 output "The Value of Variable is "{
-        value =var.myfirstvar
+        value = var.myfirstvar
 }
